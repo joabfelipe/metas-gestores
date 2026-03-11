062b2ecb05f28057b4a65639147a57d201aae7dc
Joab Felipe <64113705+joabfelipe@users.noreply.github.com>
2026-03-11 18:35:47 -0300

    Enhance email field validation in Manager schema

--- models/Manager.js
+++ models/Manager.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const ManagerSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Formato de e-mail inválido"],
+    },
     departments: { type: [String], default: [] },
     units: { type: [String], default: [] },
     accessToken: { type: String, required: true, unique: true },
