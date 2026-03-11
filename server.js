b0f3276195c4f04479074784cdd818a432f44665
Joab Felipe <64113705+joabfelipe@users.noreply.github.com>
2026-03-11 17:32:17 -0300

    Configure session store with MongoStore
    Added MongoDB session store configuration using connect-mongo.

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const path = require("path");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const Manager = require("./models/Manager");
 
@@ -39,6 +40,11 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 dia
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "sessions",
+      ttl: 60 * 60 * 24, // 1 dia em segundos
+    }),
   })
 );
 app.use(flash());
